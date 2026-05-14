6a1d9f6a033706f4c8f6f0ba3bd9e1d8c851d955
soto1231XD <alvarez.01.jlsa@gmail.com>
2026-05-14 13:33:23 -0500

    desarrollos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.cvh3dvrq038"
+    "revision": "0.0pe1jcpnjqo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
